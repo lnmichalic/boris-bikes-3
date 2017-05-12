6a3cfde9604cdb5c45a03b1b4f88bd7c50918a69
Lubos Michalic <lnmichalic@gmail.com>
2017-05-12 12:56:15 +0100

    Add stubs for Bike double and have RSpec pass all the tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,61 +1,75 @@
 require 'docking_station'
 
 describe DockingStation do
+	let (:bike)  { double :bike }
+	subject(:station) {DockingStation.new}
 
 	it "responds to release_bike" do
 	 is_expected.to respond_to :release_bike
 	end
+
 	it "releases a bike" do
-		bike = double("bike")
-		subject.dock(bike)
-		expect(subject.release_bike).to eq bike
+		allow(bike).to receive(:working?).and_return(true)
+		station.dock(bike)
+		expect(station.release_bike).to eq bike
 	end
+
 	it "Docks a bike" do
-		bike = double("bike")
-		expect(subject.dock(bike)).to eq [bike]
+		expect(station.dock(bike)).to eq [bike]
 	end
+
 	it "has a bike" do
-		expect(subject).to respond_to :bike
+		expect(station).to respond_to :bike
 	end
+
 	it "raises error if bike unavailable" do
-		expect { subject.release_bike }.to raise_error('No bikes available')
+		expect { station.release_bike }.to raise_error('No bikes available')
 	end
+
 	it "Raises error if dock is at capacity" do
-		station = DockingStation.new
-		DockingStation::DEFAULT_CAPACITY.times do
-			bike = double("bike")
-			station.dock(bike)
-		end
+		DockingStation::DEFAULT_CAPACITY.times {station.dock(bike)}
 		expect{station.dock(Bike.new)}.to raise_error("Dock is at capacity")
 	end
+
 	it "Lets us dock 20 bikes" do
-		DockingStation::DEFAULT_CAPACITY.times {subject.dock(Bike.new)}
-		expect(subject.bikes.length).to eq DockingStation::DEFAULT_CAPACITY
+		DockingStation::DEFAULT_CAPACITY.times {station.dock(Bike.new)}
+		expect(station.bikes.length).to eq DockingStation::DEFAULT_CAPACITY
 	end
+
 	it "shows that there is a default capacity" do
-		expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+		expect(station.capacity).to eq DockingStation::DEFAULT_CAPACITY
 	end
+
 	it "Shows capacity has been set by system admin" do
 		station = DockingStation.new(10)
 		expect(station.capacity).not_to eq DockingStation::DEFAULT_CAPACITY
 	end
+
 	it "Shows that the capacity can be changed" do
 		station = DockingStation.new
 		station.capacity = 21
 		expect(station.capacity).to eq 21
 	end
+
 	it "Doesn't release a broken bike" do
-		bike = double("bike")
+		allow(bike).to receive(:report_broken)
+		allow(bike).to receive(:working?).and_return(false)
 		bike.report_broken
-		subject.dock(bike)
-		expect{subject.release_bike}.to raise_error('No working bikes available')
+		station.dock(bike)
+		expect{station.release_bike}.to raise_error('No working bikes available')
 	end
+
 	it "releases only working bikes" do
-		bike = double("bike")
-		bike2 = double("bike")
-		subject.dock(bike)
+		allow(bike).to receive(:report_broken)
+		allow(bike).to receive(:working?).and_return(true)
+
+		bike2 = double(:bike)
+		allow(bike2).to receive(:report_broken)
+		allow(bike2).to receive(:working?).and_return(false)
+		station.dock(bike)
 		bike2.report_broken
-		subject.dock(bike2)
-		expect(subject.release_bike).to eq bike
+		station.dock(bike2)
+		expect(station.release_bike).to eq bike
 	end
+
 end
